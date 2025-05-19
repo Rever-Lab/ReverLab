2e36ce56f053b59af878e20b94173af78e6cb32b
Rever-Lab <reverlab0@gmail.com>
2025-05-19 19:40:33 +0200

    Update App.tsx

--- reverlab/src/App.tsx
+++ reverlab/src/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from './components/Navbar/Navbar';
 import VideoSection from './components/VideoFrom/Video';
+import Projects from './components/Projects/Projects';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
 			<>
 				<Navbar />
 				<VideoSection />
+				<Projects />
 			</>
 		</div>
   )
